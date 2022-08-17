0833adae585ed194fc29cb180cb42efefe361e7d
nahuel utn <nahuel.utn.frc.piguillem@gmail.com>
2022-08-17 13:23:05 -0300

    desk

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ app.use(cors())
 app.use(parser.json())
 
 app.get('/', (req, res) => {
-    res.send('Jira clone clone')
+    res.send('Jira clone escritorio')
 })
 app.use('/proyectos',require('./router/Proyecto.routes.js'))
 module.exports=app
